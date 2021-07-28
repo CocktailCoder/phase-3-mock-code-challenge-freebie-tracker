8fa18452837b355f3d7fa11bad437d70307533f8
cocktailcoder <iokreddit@protonmail.com>
2021-07-28 17:23:07 -0400

    update dev model methods for multiple solutions to similar problem

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -7,11 +7,23 @@ class Dev < ActiveRecord::Base
             t.item_name == item_name
         end
     end
+
+    # def received_one?(item_name)
+    #   self.freebies.find_by(item_name: item_name) == nil ? false : true
+    # end
+
     def give_away(freebie, dev)
         freebie.update(dev: dev)
       end
-      def self.examples
+    
+      # def give_away(dev:, freebie:)
+      #   freebie.dev_id = dev.dev_id
+      #   freebie.save
+      # end
+
+
+    def self.examples
         puts "hello"
-      end
+    end
 
 end
