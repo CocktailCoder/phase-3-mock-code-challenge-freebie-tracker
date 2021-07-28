5d571f232a070ab4f0835c6c6a3ac0d83c8bdcaf
cocktailcoder <iokreddit@protonmail.com>
2021-07-28 17:03:08 -0400

    adding semicolons aka keyword parameters to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,6 +5,11 @@ class Company < ActiveRecord::Base
     def give_freebie(dev, item_name, value)
         Freebies.create(item_name: item_name, value: value, dev: dev, company: self)
     end
+
+    def give_freebie(dev:, item_name:, value:)
+        #adding semicolons to the end allow you to access any value in table regardless of order
+        Freebies.create(item_name: item_name, value: value, dev_id: dev.id, company_id: self)
+    end
     
     def self.oldest_company
         Company.all.sort(:founding_year).first
